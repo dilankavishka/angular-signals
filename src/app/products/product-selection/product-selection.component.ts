9adf4b14f7b6df44f7322285bd32f7f4fae789de
dilankavishka <dkgamage01@gmail.com>
2025-12-02 13:22:25 +0530

    Changing a signals value

--- src/app/products/product-selection/product-selection.component.ts
+++ src/app/products/product-selection/product-selection.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ProductData } from '../product-data';
 import { Products } from '../product';
@@ -17,4 +17,14 @@ export class ProductSelectionComponent {
   quantity = signal(1);
 
   products = signal(ProductData.products);
+
+  onIncrease() {
+    this.quantity.update((qty) => qty + 1);
+  }
+
+  onDecrease() {
+    this.quantity.update((qty) => (qty <= 0 ? 0 : qty - 1));
+  }
+
+  eff = effect(() => console.log(this.quantity()));
 }
